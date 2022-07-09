8b15bac30775af36949eac9f4131255c7d2f29e1
dallanoce <0Dido$supermom$9weco$patty$5Ruth$Nitwit6>
2022-07-09 14:03:27 +0200

    creating window to manage a game

--- TombolaNew.py
+++ TombolaNew.py
@@ -3,50 +3,38 @@
 from PyQt6.QtCore import QAbstractTableModel, QVariant, Qt
 import pandas as pd
 
-
-
-
 from PyQt6.QtCore import QDateTime, Qt, QTimer
 from PyQt6.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
-        QDial, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
-        QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy,
-        QSlider, QSpinBox, QStyleFactory, QTableWidget, QTabWidget, QTextEdit,
-        QVBoxLayout, QWidget)
+                             QDial, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
+                             QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy,
+                             QSlider, QSpinBox, QStyleFactory, QTableWidget, QTabWidget, QTextEdit,
+                             QVBoxLayout, QWidget)
 
 
 class TombolaNew(QDialog):
     def __init__(self, parent=None):
         super(TombolaNew, self).__init__(parent)
-        styleComboBox = QComboBox()
-        styleComboBox.addItems(QStyleFactory.keys())
-
-        styleLabel = QLabel("&Style:")
-        styleLabel.setBuddy(styleComboBox)
-
-        self.useStylePaletteCheckBox = QCheckBox("&Use style's standard palette")
-        self.useStylePaletteCheckBox.setChecked(True)
-
-        disableWidgetsCheckBox = QCheckBox("&Disable widgets")
 
         self.createTopLeftGroupBox()
         self.createBottomLeftTabWidget()
-
-        disableWidgetsCheckBox.toggled.connect(self.topLeftGroupBox.setDisabled)
-        disableWidgetsCheckBox.toggled.connect(self.bottomLeftTabWidget.setDisabled)
+        self.createBottomRightGroupBox()
 
         topLayout = QHBoxLayout()
+        topLayout.addStretch(1)
 
         mainLayout = QGridLayout()
         mainLayout.addLayout(topLayout, 0, 0, 1, 2)
-        mainLayout.addWidget(self.topLeftGroupBox, 1, 0)
-        mainLayout.addWidget(self.bottomLeftTabWidget, 2, 0)
+        mainLayout.addWidget(self.bottomLeftTabWidget, 1, 0, 1, 2)
+        mainLayout.addWidget(self.topLeftGroupBox, 2, 0)
+        mainLayout.addWidget(self.bottomRightGroupBox, 2, 1)
+        """
         mainLayout.setRowStretch(1, 1)
         mainLayout.setRowStretch(2, 1)
         mainLayout.setColumnStretch(0, 1)
         mainLayout.setColumnStretch(1, 1)
+        """
+
         self.setLayout(mainLayout)
-
-
 
     def createTopLeftGroupBox(self):
         self.topLeftGroupBox = QGroupBox("Group 1")
@@ -71,7 +59,7 @@ class TombolaNew(QDialog):
     def createBottomLeftTabWidget(self):
         self.bottomLeftTabWidget = QTabWidget()
         self.bottomLeftTabWidget.setSizePolicy(QSizePolicy.Policy.Preferred,
-                QSizePolicy.Policy.Ignored)
+                                               QSizePolicy.Policy.Ignored)
 
         tab1 = QWidget()
         tableWidget = QTableWidget(10, 10)
@@ -81,20 +69,39 @@ class TombolaNew(QDialog):
         tab1hbox.addWidget(tableWidget)
         tab1.setLayout(tab1hbox)
 
-        tab2 = QWidget()
-        textEdit = QTextEdit()
-
-        textEdit.setPlainText("Twinkle, twinkle, little star,\n"
-                              "How I wonder what you are.\n" 
-                              "Up above the world so high,\n"
-                              "Like a diamond in the sky.\n"
-                              "Twinkle, twinkle, little star,\n" 
-                              "How I wonder what you are!\n")
-
-        tab2hbox = QHBoxLayout()
-        tab2hbox.setContentsMargins(5, 5, 5, 5)
-        tab2hbox.addWidget(textEdit)
-        tab2.setLayout(tab2hbox)
 
         self.bottomLeftTabWidget.addTab(tab1, "&Table")
-        self.bottomLeftTabWidget.addTab(tab2, "Text &Edit")
+
+    def createBottomRightGroupBox(self):
+            self.bottomRightGroupBox = QGroupBox("Group 3")
+            self.bottomRightGroupBox.setCheckable(True)
+            self.bottomRightGroupBox.setChecked(True)
+
+            lineEdit = QLineEdit('s3cRe7')
+            lineEdit.setEchoMode(QLineEdit.EchoMode.Password)
+
+            spinBox = QSpinBox(self.bottomRightGroupBox)
+            spinBox.setValue(50)
+
+            dateTimeEdit = QDateTimeEdit(self.bottomRightGroupBox)
+            dateTimeEdit.setDateTime(QDateTime.currentDateTime())
+
+            slider = QSlider(Qt.Orientation.Horizontal, self.bottomRightGroupBox)
+            slider.setValue(40)
+
+            scrollBar = QScrollBar(Qt.Orientation.Horizontal, self.bottomRightGroupBox)
+            scrollBar.setValue(60)
+
+            dial = QDial(self.bottomRightGroupBox)
+            dial.setValue(30)
+            dial.setNotchesVisible(True)
+
+            layout = QGridLayout()
+            layout.addWidget(lineEdit, 0, 0, 1, 2)
+            layout.addWidget(spinBox, 1, 0, 1, 2)
+            layout.addWidget(dateTimeEdit, 2, 0, 1, 2)
+            layout.addWidget(slider, 3, 0)
+            layout.addWidget(scrollBar, 4, 0)
+            layout.addWidget(dial, 3, 1, 2, 1)
+            layout.setRowStretch(5, 1)
+            self.bottomRightGroupBox.setLayout(layout)
